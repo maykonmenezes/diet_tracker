66865de499fdf46f18dafed297126b7b14c653cc
Maykon Menezes <maykon.meneses@gmail.com>
2020-04-11 11:59:12 -0300

    Add validations and relatioship for user and diet models

--- app/models/diet.rb
+++ app/models/diet.rb
@@ -1,2 +1,6 @@
 class Diet < ApplicationRecord
+  belongs_to :user
+
+  validates :start, :end, :init_weight, :height, :ideal_weight, presence: true
+  validates_numericality_of :init_weight, :height, :ideal_weight, greater_than: 0.0 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :weights
+  has_many :diets
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
